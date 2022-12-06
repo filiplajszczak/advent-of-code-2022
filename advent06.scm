3212d9ce1d28ed8619e58ba8258b1812795508a7
Filip Lajszczak <filip@lajszczak.dev>
2022-12-06 07:48:45 +0000

    day 6

---
+++ advent06.scm
@@ -0,0 +1,48 @@
+#!/usr/bin/guile -s
+!#
+
+(use-modules (srfi srfi-1)
+             (srfi srfi-64)
+             ((f))
+             ((algorithms)))
+
+(define (scan lst index size)
+  (if (eq? ((compose length char-set->list list->char-set) (car lst)) size)
+      index
+      (scan (cdr lst) (+ index 1) size)))
+
+(define (solve data size)
+  (scan (sliding (string->list data) size 1) size size))
+
+(define (part-1 filename)
+  (solve (input filename) 4))
+
+(define (part-2 filename)
+  (solve (input filename) 14))
+
+(define (input filename)
+  (string-trim-right (read-text filename)))
+
+
+;; Part 1
+(display (part-1 "day06.in"))
+(newline)
+;; Part 2
+(display (part-2 "day06.in"))
+(newline)
+
+;; Tests
+(test-begin "example")
+
+(test-equal "Test part 1-1" 7 (part-1 "day06-example-1.in"))
+(test-equal "Test part 1-2" 5 (part-1 "day06-example-2.in"))
+(test-equal "Test part 1-3" 6 (part-1 "day06-example-3.in"))
+(test-equal "Test part 1-4" 10 (part-1 "day06-example-4.in"))
+(test-equal "Test part 1-5" 11 (part-1 "day06-example-5.in"))
+(test-equal "Test part 1-1" 19 (part-2 "day06-example-1.in"))
+(test-equal "Test part 1-2" 23 (part-2 "day06-example-2.in"))
+(test-equal "Test part 1-3" 23 (part-2 "day06-example-3.in"))
+(test-equal "Test part 1-4" 29 (part-2 "day06-example-4.in"))
+(test-equal "Test part 1-5" 26 (part-2 "day06-example-5.in"))
+
+(test-end "example")
